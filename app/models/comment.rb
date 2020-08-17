deff35e77fbdcc20004d5c7a6b49ceb60bef244a
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-18 00:00:09 +0300

    add comments model with validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,2 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  validates :body, presence: true
-  validates :user_id, presence: true
-  validates :post_id, presence: true
-  validates_associated :user, :post
 end
